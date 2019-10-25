2ce42914d67cd9a2258db3af8a0092a2acb6e7bf
jeanhbr <jean.haberer@bde-insa-lyon.fr>
2019-10-25 09:07:45 +0200

    Direction

--- Grid.pl
+++ Grid.pl
@@ -16,16 +16,19 @@ displayGrid(Grid,Player) :-
     forall(between(1, Nblines, Line), (nth1(Line,Grid,CharList), displayRow(CharList), nl)),
     nl.
 
-
 displayRow(CharList) :- 
     length(CharList,Nbcol),
     write("  "),
     forall(between(1, Nbcol, Col),(nth1(Col,CharList,Char), write(Char), write(" "))). 
 
-
 doMove(Grid,Line,Column,Player,NewGrid) :-
     fail.
 
+direction(bas,Lin,Col,NextLin,NextCol) :- incr(Lin,NextLin), NextCol = Col.
+direction(basDroite,Lin,Col,NextLin,NextCol) :- incr(Lin,NextLin), incr(Col,NextCol).
+direction(droite,Lin,Col,NextLin,NextCol) :- incr(Col,NextCol), NextLin = Lin.
+direction(hautDroite,Lin,Col,NextLin,NextCol) :- incr(Lin,NextLin), incr(NextCol,Col).
+direction(haut,Lin,Col,NextLin,NextCol) :- incr(NextLin,Lin), NextCol = Col.
 
 moveRight(Grid,Line,Col,NewGrid) :- 
     incr(Col,Col1),
