25aba91c51a5f6b5681ca1b525ec099397f3acb6
jeanhbr <jean.haberer@bde-insa-lyon.fr>
2019-10-25 08:24:06 +0200

    Yolo

--- Grid.pl
+++ Grid.pl
@@ -3,8 +3,8 @@ initialGrid([
 ["1", "-", "-", "-", "-", "-", "-", "-", "-"],
 ["2", "-", "-", "-", "-", "-", "-", "-", "-"],
 ["3", "-", "-", "-", "-", "-", "-", "-", "-"],
-["4", "-", "-", "-", o,   x,   "-", "-", "-"],
-["5", "-", "-", "-", x,   o,   "-", "-", "-"],
+["4", "-", "-", "-",  o,  x,   "-", "-", "-"],
+["5", "-", "-", "-",  x,  o,   "-", "-", "-"],
 ["6", "-", "-", "-", "-", "-", "-", "-", "-"],
 ["7", "-", "-", "-", "-", "-", "-", "-", "-"],
 ["8", "-", "-", "-", "-", "-", "-", "-", "-"]
@@ -12,7 +12,7 @@ initialGrid([
 
 displayGrid(Grid,Player) :-
     length(Grid,Nblines),
-    nl, write("  AU TOUR DE : "), write(Player), nl, nl,
+    nl, write("  AU TOUR DES "), write(Player), nl, nl,
     forall(between(1, Nblines, Line), (nth1(Line,Grid,CharList), displayRow(CharList), nl)),
     nl.
 
@@ -21,3 +21,31 @@ displayRow(CharList) :-
     length(CharList,Nbcol),
     write("  "),
     forall(between(1, Nbcol, Col),(nth1(Col,CharList,Char), write(Char), write(" "))). 
+
+
+doMove(Grid,Line,Column,Player,NewGrid) :-
+    fail.
+
+
+moveRight(Grid,Line,Col,NewGrid) :- 
+    incr(Col,Col1),
+    element(Grid,Line,Col,Token1),
+    element(Grid,Line,Col1,Token2),
+    canFlip(Token1,Token2),
+    moveRight(Grid,Line,Col1,NewGrid).
+
+moveRight(Grid,Line,Col,NewGrid) :- 
+    incr(Col,Col1),
+    element(Grid,Line,Col,Token1),
+    element(Grid,Line,Col1,Token2),
+    NewGrid=Grid,
+    equal(Token1,Token2).
+
+element(Grid,Line,Col,Element) :- nth1(Line,Grid,CharList), nth1(Col,CharList,Element).
+
+equal(Token,Token).
+canFlip(x,o).
+canFlip(o,x).
+
+incr(X, X1) :-
+    X1 is X+1.
