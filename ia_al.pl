707f4ff73e9226501f62aa0a8a0b44a01f7f6e40
jeanhbr <jean.haberer@bde-insa-lyon.fr>
2019-11-03 17:51:37 +0100

    Déchet supprimé

--- ia_al.pl
+++
@@ -1,5 +0,0 @@
-% IA Aleatoire
-ia_al(grid, line, col, player) :- repeat, random(1,9,line), random(1,9, col), isValidMove(grid, line, col, player), doMove(grid, line, col, player, newGrid).
-
-line(1) :- line(a).
-
