32ca272a345b91449cdcdf43a0a522b8ad24f6a4
jeanhbr <jean.haberer@bde-insa-lyon.fr>
2019-11-05 17:18:42 +0100

    Heuristic

--- play_minmax.pl
+++ play_minmax.pl
@@ -47,23 +47,3 @@ all_possible_moves(Token, Grid, AllMoves) :-
    isValidMove(Grid,Line,Column,Token),
    AllMoves).
 all_possible_moves(_, _, []).
-
-%to improve performance we cache the results and try to retrieve it
-
-heuristic(Heur,Grid,Token,Eval) :-
-    variant_hash([Heur,Grid,Token],Key),
-    get(Key, Eval).
-
-heuristic(Heur,Grid,Token,Eval) :-
-    next(Token,Opponent),
-    variant_hash([Heur,Grid,Opponent],Key),
-    get(Key, OpponentEval),
-    Eval is OpponentEval * -1.
-
-heuristic(h1,Grid,Token,Eval) :-
-    countTokens(Grid, Token, 0, NbTokensCurrentPlayer),
-    nextPlayer(Token,Opponent),
-    countTokens(Grid, Opponent, 0, NbTokensOpponent),
-    Eval is NbTokensCurrentPlayer-NbTokensOpponent,
-    variant_hash([h1,Grid,Token],Key),
-    set(Key, Eval).
